7aae437c04b198732e446ab87f1c154819edc1ca
ayushjii <pawangoyal6789@gmail.com>
2022-07-20 03:07:26 +0530

    F

--- src/App.js
+++ src/App.js
@@ -9,12 +9,20 @@ import { Footer } from "./components/Footer";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup.js";
 import Allitem from "./components/Allitems";
+import Thought from "./components/Thoughts";
 
 
  export default function App() {
   return (
       <Router>
         <NavBar />
+        
+        {/* <form action="/post" method="post" >
+        <label for="firstName">First Name</label>
+        <input type="text" id="firstName" name="fName" required />
+          <button type="submit">Connected?</button>
+        </form> */}
+   
         <div className="pages">
           <Routes>
             <Route exact path="/" element={<Home/>} />
@@ -24,6 +32,7 @@ import Allitem from "./components/Allitems";
             <Route path="/Signin" element={<Signin/>} />
             <Route path="/Signup" element={<Signup/>} />
             <Route path="/all" element={<Allitem/>} />
+            <Route path="/all/dis" element={<Thought/>} />
           </Routes>
         </div>
         <Footer/>
---
+++ src/components/Thoughts.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ThoughtStyles = styled.div`
+margin: 5rem 10rem;
+.left{
+    text-align:left;
+}
+.right{
+    text-align:center;
+     ${'' /* margin:3rem; */}
+}
+.heading{
+    font-size:80px;
+    color:#000;
+}
+.about_dis{
+    font-size:25px;
+    color: blue;
+}
+.dis_container{
+    display:flex;
+    gap: 5rem;
+    margin-top:7rem;
+    position:relative;
+    
+}
+.dis_image{
+    height:300px;
+    width:300px;
+    border-radius:10px;
+    max-width:100%;
+    }
+    
+@media only screen and (max-width: 500px) {
+    .dis_container{
+        flex-direction:column;
+    } 
+    .right{
+      text_align:center;
+    }
+}
+`
+export default function Thought (
+    {
+    CardImage='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYtfZRhbGQtq2BapB2MXJfWIO2QriO5Wx3qQ&usqp=CAU',
+    dis_heading='hello',
+    dis_about='rce  k  l.   w o o oio eow  io oiwe owe  . n; ew; w k. k  /e  kl k    e ee  '
+    }
+) {
+    return(
+        <ThoughtStyles>
+        <div className='dis_container'>
+          <div className='left'>
+            <div className='heading'>
+                {dis_heading}
+            </div>
+            <div className='about_dis'>
+                {dis_about}
+            </div>
+          </div>
+          <div className='right'>
+          <img className='dis_image' src={CardImage}  alt=""/>
+            
+          </div>
+        </div>
+        </ThoughtStyles>
+    );
+}
